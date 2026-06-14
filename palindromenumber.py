69296e0f44660d19c05af937e60647c1228c2df3
danielkwan-dev <d35kwan@uwaterloo.ca>
2026-06-13 20:43:19 -0700

    maximumSubArray

--- palindromenumber.py
+++ palindromenumber.py
@@ -1,10 +1,17 @@
 class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        num = str(x)
-        return num == num[::-1]
+    def maxSubArray(self, nums):
+        left = 0
+        max_sum = nums[0]
+        curr_sum = 0
 
-        # convert num to str and check if it's reverse ([::-1]) is equal
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+
+            if curr_sum > max_sum:
+                max_sum = curr_sum
+            
+            if curr_sum < 0:
+                curr_sum = 0
+                left = i + 1
+        
+        return max_sum
