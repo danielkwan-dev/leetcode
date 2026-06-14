dcf0911a02646ae65fc1aefab70ed7f04ffa5701
danielkwan-dev <d35kwan@uwaterloo.ca>
2026-06-13 20:52:45 -0700

    mySqrt

--- palindromenumber.py
+++ palindromenumber.py
@@ -1,17 +1,23 @@
 class Solution(object):
-    def maxSubArray(self, nums):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
         left = 0
-        max_sum = nums[0]
-        curr_sum = 0
-
-        for i in range(len(nums)):
-            curr_sum += nums[i]
-
-            if curr_sum > max_sum:
-                max_sum = curr_sum
-            
-            if curr_sum < 0:
-                curr_sum = 0
-                left = i + 1
+        right = x
         
-        return max_sum
+        while left <= right:
+            mid = (left + right)//2
+            if mid * mid < x:
+                left = mid + 1
+            elif mid * mid > x:
+                right = mid - 1
+            else:
+                return mid
+        
+        return right
+        
+        
+
+        
