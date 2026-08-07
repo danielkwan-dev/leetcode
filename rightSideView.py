8543f260e15e3aee745d6daf933a014237760540
danielkwan-dev <d35kwan@uwaterloo.ca>
2026-08-06 21:37:10 -0700

    rightSideView

---
+++ rightSideView.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+
+        if not root:
+            return []
+        queue = [root]
+        result = []
+
+        while queue:
+            n = len(queue)
+            for i in range(n):
+                node = queue.pop(0)
+                if i == n - 1:
+                    result.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+        return result
+        
