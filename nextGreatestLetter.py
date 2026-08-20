9baa427ae53f6af21e2d3ff4ec60ee77b02e37cb
danielkwan-dev <d35kwan@uwaterloo.ca>
2026-08-19 21:20:09 -0700

    nextGreatestLetter

---
+++ nextGreatestLetter.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        for letter in letters:
+            if target < letter:
+                return letter
+        
+        return letters[0]
